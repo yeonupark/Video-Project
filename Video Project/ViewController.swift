a519e4475fba1e8f7f11e6ad67b9badbd5d69750
yeonupark <idepix@naver.com>
2023-08-17 16:18:52 +0900

    Initial Commit

---
+++ Video Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Video Project
+//
+//  Created by 마르 on 2023/08/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
